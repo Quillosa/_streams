98446d79495f710f9bbb8a002d1a22a0b4b4b840
pogi0 <denden171.quillosa@gmail.com>
2023-11-10 23:41:08 +0800

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace program;
+    class myFirstProgram {
+
+
+        static void Main(string[] args) {
+
+        Console.ForegroundColor = ConsoleColor.Green;
+        //Console.BackgroundColor = ConsoleColor.White;
+
+            string data;
+            StreamReader reader = null;
+            StreamWriter writer = null;
+
+        try
+        {
+            reader = new StreamReader("C:\\Users\\Public\\Documents\\letter.txt");
+           
+             data = reader.ReadLine();
+
+            while (data != null)
+            {
+                Console.WriteLine(data);
+                data = reader.ReadLine();
+            }
+            reader.Close();
+            writer = new StreamWriter("C:\\Users\\Public\\Documents\\letter.txt");
+            writer.WriteLine(@"BARUYYYYYyyyyy");
+
+        }catch(Exception ex)
+        {
+            Console.WriteLine("may error ang dagis");
+        }
+        finally
+        {   
+            writer.Close();
+        }
+     
+        }
+
+     }
